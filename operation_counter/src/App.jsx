18750cec3df717ce9c2639494a47173da9c654ec
hatnim <dlgotsla3391@gmail.com>
2024-10-27 14:48:20 +0900

    counter refactoring

--- operation_counter/src/App.jsx
+++ operation_counter/src/App.jsx
@@ -2,55 +2,47 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [inputNum, setInputNum] = useState('');
+  const [inputNumber, setInputNum] = useState('');
   const [result, setResult] = useState(0);
+  const operators = ['+', '-', '*', '/', '%'];
+
+  const handleCalculate = (operator) => {
+    switch (operator) {
+      case '+':
+        setResult((prev) => prev + inputNumber);
+        break;
+      case '-':
+        setResult((prev) => prev - inputNumber);
+        break;
+      case '*':
+        setResult((prev) => prev * inputNumber);
+        break;
+      case '/':
+        setResult((prev) => prev / inputNumber);
+        break;
+      case '%':
+        setResult((prev) => prev % inputNumber);
+        break;
+    }
+  };
+
   return (
     <div className="container">
       <input
         className="input"
         type="number"
-        value={inputNum}
+        value={inputNumber}
         placeholder="숫자를 입력하세요"
         onChange={(event) => {
           setInputNum(Number(event.target.value));
         }}
       />
       <div>
-        <button
-          onClick={() => {
-            setResult((prev) => prev + inputNum);
-          }}
-        >
-          +
-        </button>
-        <button
-          onClick={() => {
-            setResult((prev) => prev - inputNum);
-          }}
-        >
-          -
-        </button>
-        <button
-          onClick={() => {
-            setResult((prev) => prev * inputNum);
-          }}
-        >
-          *
-        </button>
-        <button
-          onClick={() => {
-            setResult((prev) => prev / inputNum);
-          }}
-        >
-          /
-        </button>
-        <button
-          onClick={() => {
-            setResult((prev) => prev % inputNum);
-          }}
-        >
-          %
-        </button>
+        {operators.map((operator) => (
+          <button key={operator} onClick={() => handleCalculate(operator)}>
+            {operator}
+          </button>
+        ))}
       </div>
       <p>계산결과: {result}</p>
     </div>
